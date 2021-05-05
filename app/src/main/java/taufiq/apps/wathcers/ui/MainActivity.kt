21a36667ee8097fd29e49159a98c73bcb1ff8a46
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2021-05-05 12:04:06 +0800

    creating menu to navigate into fav page

--- app/src/main/java/taufiq/apps/wathcers/ui/MainActivity.kt
+++ app/src/main/java/taufiq/apps/wathcers/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package taufiq.apps.wathcers.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,7 +14,7 @@ import taufiq.apps.wathcers.utils.ViewPagerAdapter
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,11 +22,27 @@ class MainActivity : AppCompatActivity() {
 
         val sectionsPagerAdapter = ViewPagerAdapter(this)
         binding.viewPager.adapter = sectionsPagerAdapter
-        TabLayoutMediator(binding.tabsId,binding.viewPager) { tab , position->
+        TabLayoutMediator(binding.tabsId, binding.viewPager) { tab, position ->
             tab.text = resources.getString(TAB_TITLES[position])
         }.attach()
 
         binding.tabsId.isTabIndicatorFullWidth = true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_favorite ->{
+                Toast.makeText(this, "Show favorite", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 
     companion object {
@@ -31,5 +50,6 @@ class MainActivity : AppCompatActivity() {
             R.string.tab_text_1,
             R.string.tab_text_2
         )
+
     }
 }
